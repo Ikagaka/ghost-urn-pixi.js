952ffd8df45927a50b246b50fa2c82b2010e3d10
Narazaka <info@narazaka.net>
2017-04-16 11:24:23 +0900

    offset

--- src/web/t.tsx
+++ src/web/t.tsx
@@ -16,7 +16,7 @@ ReactDOM.render(
                 <Named>
                     <Scope>
                         <ScopeBalloon x={100} y={50}>
-                            <BalloonSurface image="foo.png" offsetLeft={10} offsetTop={10} scrollOffset={0}>
+                            <BalloonSurface image="foo.png" offsetLeft={10} offsetTop={10} offsetRight={10} offsetBottom={10} scrollOffset={7}>
                                 \f[height,12pt]a\f[height,8pt]a\f[height,2em]a\f[color,0,0,128]aa\\d\n
                                 q\f[name,Meiryo]aji\f[bold,1]sjan
                                 \_l[@2em,45]あいうえお\f[default]あいうえおあいうえおあいうえおあいうえお
